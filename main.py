6f9827ff44e401e2e29405be24a9cd513016e446
bowl23 <1467059904@qq.com>
2024-02-10 13:58:17 +0800

    输入框

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-# from PyQt5.QtCore import
+from PyQt5.QtGui import QFont
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QTextEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QLineEdit, QVBoxLayout, QSizePolicy
 
 
 class Mywindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        layout = QGridLayout()
-        editline = QTextEdit()
-        btns = ['1', '2', '3','+', '4', '5', '6','-', '7', '8', '9','*','0','C','.','/']
+        self.resize(400, 500)
+        main_layout = QVBoxLayout()
+        btns_layout = QGridLayout()
+        btns = ['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', '0', 'C', '.', '/']
         for i in range(len(btns)):
             btn = QPushButton(str(btns[i]))
-            layout.addWidget(btn, i // 4, i % 4)
-        self.setLayout(layout)
+            btns_layout.addWidget(btn, i // 4, i % 4)
+        input_font=QFont()
+        input_font.setPointSize(50)
+        inputbox = QLineEdit()
+        inputbox.setFont(input_font)
+        inputbox.setFixedHeight(100)
+        main_layout.addWidget(inputbox)
+        main_layout.addLayout(btns_layout)
+        self.setLayout(main_layout)
 
 
 if __name__ == '__main__':
